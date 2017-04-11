e949e93eb06523f9861115d67d672eee6b9a95ae
Bernabe <bernabe@goout.mx>
2017-04-10 20:46:44 -0500

    Initial App Structure

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,16 @@
+import React, {
+  Proptypes
+}
+from 'react';
+
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Courses Page</h1>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
