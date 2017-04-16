a2201cb1e812ff16cdb76f7978a406887672989e
Bernabe <bernabe@goout.mx>
2017-04-16 13:50:41 -0500

    Separate markup from container and styled table courses.

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -11,21 +11,18 @@ import {
   bindActionCreators
 }
 from 'redux';
+import CourseList from './courseList';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
   }
 
-  courseRow(course, index) {
-    return <div key={index}> {course.title} </div>;
-  }
-
   render() {
     return (
       <div>
         <h1>Courses Page</h1>
-        {this.props.courses.map(this.courseRow)}
+        <CourseList courses={this.props.courses}/>
       </div>
     );
   }
